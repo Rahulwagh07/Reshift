c4b492cbc0f30f703d2744d20f7f2b6a473efcc0
Rahulwagh07 <rahulwagh3774@gmail.com>
2024-04-17 21:25:53 +0530

    added middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,65 +1,99 @@
 import { NextResponse } from 'next/server';
-import {jwtVerify } from 'jose';
+import { jwtVerify } from 'jose';
 import { getJwtSecretKey } from './lib/getJwtSecretKey';
 import type { NextRequest } from 'next/server';
- 
+
+const loginSignupPaths = ['/login', '/signup'];
+
+const handleUnauthenticated = (request: NextRequest) => {
+  if (!loginSignupPaths.includes(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+  return NextResponse.next();
+};
+
+const handleAuthenticated = (request: NextRequest) => {
+  if (loginSignupPaths.includes(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+  return NextResponse.next();
+};
+
+const handleAuthorization = async (request: NextRequest, token: string) => {
+  try {
+    const { payload } = await jwtVerify(token, getJwtSecretKey());
+    const newHeaders = new Headers(request.headers);
+    const userId = (payload as { id: string }).id;
+    const email = (payload as { email: string }).email;
+    const accountType = (payload as { accountType: string }).accountType;
+
+    newHeaders.set('userId', userId);
+    newHeaders.set('email', email);
+    newHeaders.set('accountType', accountType);
+
+    const isMemberPath = request.nextUrl.pathname.startsWith('/dashboard');
+    const isAdminPath = request.nextUrl.pathname.startsWith('/admin');
+
+    if (isMemberPath && accountType === 'Admin') {
+      return NextResponse.json(
+        { message: 'Admins cannot access members dashboard paths.' },
+        { status: 403 }
+      );
+    }
+
+    if (isAdminPath && accountType === 'Member') {
+      return NextResponse.json(
+        { message: 'Members cannot access admin paths.' },
+        { status: 403 }
+      );
+    }
+
+    return NextResponse.next({
+      request: {
+        headers: newHeaders,
+      },
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: 'JWT VERIFICATION FAILED',
+    });
+  }
+};
+
 export async function middleware(request: NextRequest) {
-    let token: string | undefined;
-    const body = await request.text();
-    
-    let requestBody;
-    try {
-        requestBody = JSON.parse(body);
-    } catch (error) {
-        // console.error('Error parsing request body:', error);
-    }
-    token = requestBody?.token;
+  const token =
+    request.cookies.get('token')?.value ||
+    request.headers
+      .get('Authorization')
+      ?.replace('Bearer ', '')
+      .replace(/"/g, '');
 
-    // If not found in body  check cookies and headers
-    if (!token) {
-    token = request.cookies.get('token')?.value ||
-            request.headers.get('Authorization')?.replace('Bearer ', '');
-    }
-     
-    // if(!token){
-    //     return NextResponse.json({
-    //         message: "Token not Found",
-    //     }, {status: 401})
-    // }
+  if (!token) {
+    return handleUnauthenticated(request);
+  }
 
-    if (token) {
-        try {
-            const { payload } = await jwtVerify(token, getJwtSecretKey());
-            const newHeaders = new Headers(request.headers)
-            const userId = (payload as { id: string }).id;
-            const email = (payload as { email: string }).email;
-            const accountType = (payload as {accountType:string }).accountType;
-            newHeaders.set('userId', userId);
-            newHeaders.set('email', email);
-            newHeaders.set('accountType', accountType);
-    
-            // And produce a response with the new headers
-            return NextResponse.next({
-            request: {
-                // New request headers
-                headers: newHeaders,
-            },
-            })
-        } catch (error) {
-            return NextResponse.json({
-                success: false,
-                message: 'JWT VERIFICATION FAILED',
-            });
-        }
-    }   
+  if (token) {
+    const authenticated = handleAuthenticated(request);
+    if (authenticated.status !== 200) {
+      return authenticated;
+    }
+
+    return handleAuthorization(request, token);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        '/dashboard',
-        '/admin/project',
-        '/api/admin/project',
-        '/api/admin/createTask',
-        '/api/acceptinvite',
-    ],
+  matcher: [
+    '/dashboard',
+    '/admin/project',
+    '/api/admin/project',
+    '/api/admin/createTask',
+    '/api/acceptinvite',
+    '/admin/dashboard',
+    '/login',
+    '/signup',
+  ],
 };
